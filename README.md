7c6add8f281a98d11dcfb2d37af896ee661fadf6
paulduschek <72552058+paulduschek@users.noreply.github.com>
2022-10-10 12:19:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-mmygaw)
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Guten Tag!</h1>
+    <!--img alt="Vue logo" src="https://vuejs.org/images/logo.png">-->
+    <HelloWorld msg="5BHIT" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
